78f57ccba824a0e8b5929677ca9dd59dd5beebe2
akshat2310 <56287654+akshat2310@users.noreply.github.com>
2020-07-18 09:35:41 +0530

    Add files via upload

---
+++ SortFileName.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class SortFileName {
+    public static void main(String[] args) {
+        File fileDir = new File("C:/Users/akshat/Desktop/out/production");
+        if(fileDir.isDirectory()){
+            List listFile = Arrays.asList(fileDir.list());
+            System.out.println("Listing files unsorted");
+            for(String s:listFile){
+                System.out.println(s);
+            }
+            Collections.sort(listFile);
+            System.out.println("---------------------------------------");
+            System.out.println("Sorting by filename in ascending order");
+            for(String s:listFile){
+                System.out.println(s);
+            }
+            System.out.println("---------------------------------------");
+            System.out.println("Sorting by filename in descending order");
+            Collections.sort(listFile,Collections.reverseOrder());
+            for(String s:listFile){
+                System.out.println(s);
+            }
+
+        }
+        else{
+            System.out.println(fileDir.getAbsolutePath() + " is not a directory");
+        }
+
+    }
+
+}
